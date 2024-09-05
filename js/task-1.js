164d9465b5aeeffa1aa5ade160533658de25c315
Andrij P. <andrejpopkov565@gmail.com>
2024-09-05 21:31:14 +0300

    .

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,20 @@
-function slugify(title) {
-  var slug = title.toLowerCase().split(" ").join("-");
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  let arrayValuesProducts = Object.values(products);
+  let sumProducts = 0;
+  for (let i = 0; i < arrayValuesProducts.length; i++) {
+    sumProducts += arrayValuesProducts[i];
+  }
+  if (sumProducts < containerSize) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
